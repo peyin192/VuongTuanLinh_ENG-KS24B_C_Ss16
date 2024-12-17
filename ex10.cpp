ba4eb02759df79c6b74d3d7c32e0a8a7f766fd20
peyin192 <vuonglinh190206.qv@gmail.com>
2024-12-17 09:25:34 +0700

    Upload ex10.cpp

--- ex10.cpp
+++ ex10.cpp
@@ -1,37 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
+int addElement(int *arr,int index, int *n);
 
-void deleteArr(int *arr, int index, int length);
-
-int main(void) {
-    int arr[] = {1, 2, 3, 4, 5};
-    int length = sizeof(arr) / sizeof(arr[0]);
-
-    for (int i = 0; i < length; i++) {
-        printf("%d ", arr[i]);
-    }
-
-    int index;
-    printf("\nMoi ban nhap vi tri muon xoa: ");
-    scanf("%d", &index);
-
-    if (index < 0 || index >= length) {
-        printf("Vi tri khong hop le\n");
-        return 1;
-    }
-
-    deleteArr(arr, index, length);
-
-    for (int i = 0; i < length - 1; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-
-    return 0;
+//arr = realloc(arr,(*n+1)*sizeof(int));
+int main(){
+	int *arr;
+	int n;
+	
+	printf("Nhap so phan tu: ");
+	scanf("%d",&n);
+	
+	arr = (int *)malloc(n*sizeof(int));
+	for(int i=0;i<n;i++){
+		printf("Nhap phan tu thu %d: ",i+1);
+		scanf("%d",arr+i);
+	}
+	addElement(arr,3,&n);
+	
+	
+	
+	for(int i=0;i<n;i++){
+		printf("%d \t",*(arr+i));
+	}
+	return 0;
 }
 
-void deleteArr(int *arr, int index, int length) {
-    for (int i = index; i < length - 1; i++) {
-        arr[i] = arr[i + 1];
-    }
-}
+
+  addElement(int *arr,int index, int *n){
+  	if(index<0||index>*n){
+  		printf("Vi tri them khong hop le");
+  		
+  		return 0;
+  		
+	  }
+  	arr = realloc(arr,(*n -1)*sizeof(int));
+  	for(int i=index;i<*n;i++){
+  		*(arr+i)=*(arr+i+1);
+	  }
+
+        (*n)--;
+  }
+
 
